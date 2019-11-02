cda0916d51885465d078d91981d457b8c8dd44b5
root <root@localhost.localdomain>
2019-11-02 18:16:51 +0300

    1. добавлен уровень статов для человека 2. добавлены возможности получать урон и восстанавливать здоровье для человека

--- humans.py
+++ humans.py
@@ -1,5 +1,3 @@
-
-
 class Humans():
 
 
@@ -12,7 +10,16 @@ class Humans():
         self.stamina = stamina
         self.iq = iq
         self.magic = magic
+        self.level_health = self.health
+        self.level_force = 100
+        self.level_stamina = 100
+        self.level_iq = 100
+        self.level_magic = 100
         self.money = money
+
+    def damage(self, damage):
+        self.level_health -= damage
+
     def upgrade_force(self):
         self.force += 1
 
@@ -24,3 +31,8 @@ class Humans():
 
     def upgrade_magic(self):
         self.magic += 1
+
+    def regeniration(self):
+        if self.level_health <= self.health-10:
+            self.level_magic -= self.level_magic/2
+            self.level_health += int(0.1 * self.health)
--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import orgs
 
 
 hero = humans.Humans(20, "Den", "Human", 100, 1, 1, 1, 1, 100 )
-a = 0
 
 
-print(hero.age)
-hero.upgrade_force()
+
+hero.damage(30)
+hero.regeniration()
+hero.regeniration()
+print(hero.level_health)
+print(hero.level_magic)
