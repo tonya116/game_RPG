7f31dc398f471632d921ff809b15224fa4c3b976
root <root@localhost.localdomain>
2019-11-02 18:29:28 +0300

    1. добавлен графический интерфейс  tkinter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import humans
 import orgs
+import tkinter
+from tkinter import *
 
 
 
@@ -12,3 +14,14 @@ hero.regeniration()
 hero.regeniration()
 print(hero.level_health)
 print(hero.level_magic)
+
+
+root = Tk()
+root.geometry("500x500")
+
+canvas = Canvas(root, width = 500, height = 500, bg = 'black')
+
+
+
+canvas.pack()
+root.mainloop()
