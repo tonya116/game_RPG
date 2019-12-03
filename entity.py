f09f97403a5f551a484e119a5d5eaab09fbbee65
root <root@localhost.localdomain>
2019-12-03 17:59:30 +0300

    1. уухххууу, все работает на tkinter'e 2. пока что просто создание объекта

--- entity.py
+++ entity.py
@@ -1,22 +1,9 @@
-from tkinter import *
-
-WIDTH = 640
-HEIGHT = 480
-
-
 
 
 class Entity():
 
     def __init__(self, age, name, race, health, force, stamina, iq, magic, money):
-        self.root = Tk()
-        self.canvas = Canvas(self.root, width=WIDTH, height=HEIGHT, bg='black')
-        self.canvas.pack()
-        self.color = "green"
-        self.x = 100
-        self.y = 100
-        self.rect_id = self.canvas.create_rectangle(self.x, self.y, self.x + 10, self.y + 10, fill=self.color)
-        self.root.mainloop()
+
         self.age = age
         self.name = name
         self.race = race
@@ -35,14 +22,19 @@ class Entity():
     def damage(self, damage):
         self.level_health -= damage
 
+    def upgrade_force(self):
+        self.force += 1
+
+    def upgrade_stamina(self):
+        self.stamina += 1
+
+    def upgrade_iq(self):
+        self.iq += 1
+
+    def upgrade_magic(self):
+        self.magic += 1
 
     def regeneration(self):
         if self.level_health <= self.health-10:
             self.level_magic -= self.level_magic/2
             self.level_health += int(0.1 * self.health)
-
-    def tick(self):
-        after(50, tick)
-
-
-#obj = Entity(20, "Den", "Human", 100, 1, 1, 1, 1, 100)
--- humans.py
+++ humans.py
@@ -1,52 +1,5 @@
 import showing
-import keyboard
-
-class Humans():
 
 
-    def __init__(self, age, name, race, health, force, stamina, iq, magic, money):
-
-        self.age = age
-        self.name = name
-        self.race = race
-        self.health = health
-        self.force = force
-        self.stamina = stamina
-        self.iq = iq
-        self.magic = magic
-        self.level_health = self.health
-        self.level_force = 100
-        self.level_stamina = 100
-        self.level_iq = 100
-        self.level_magic = 100
-        self.money = money
-
-
-
-
-
-
-
-    def damage(self, damage):
-        self.level_health -= damage
-
-    def upgrade_force(self):
-        self.force += 1
-
-    def upgrade_stamina(self):
-        self.stamina += 1
-
-    def upgrade_iq(self):
-        self.iq += 1
-
-    def upgrade_magic(self):
-        self.magic += 1
-
-    def regeneration(self):
-        if self.level_health <= self.health-10:
-            self.level_magic -= self.level_magic/2
-            self.level_health += int(0.1 * self.health)
-
-
-
-    
+class Humans(showing.Entity):
+    pass
